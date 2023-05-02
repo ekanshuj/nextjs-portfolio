1c6dc0f575a450566b6490f77dcdbdaaa5695bf9
Ekanshu Joshi <99767187+ekanshuj@users.noreply.github.com>
2023-05-03 04:24:25 +0530

    updated-config-file

--- config/config.ts
+++ config/config.ts
@@ -48,28 +48,28 @@ export const stuffs = [
     name: "first-contributions",
     url: "https://github.com/ekanshuj/first-contributions",
     description:
-      "First Contributions is a beginner-friendly GitHub repository that provides a step-by-step guide for making your first contribution to an open-source project and can be used to showcase your involvement in the open-source community on your portfolio website",
+      "First Contributions is a beginner-friendly GitHub repository that provides a step-by-step guide for making your first contribution to an open-source project and can be used to showcase your involvement in the open-source community on your portfolio website.",
   },
   {
     icon: dots,
-    name: "youtube-ui-clone",
-    url: "https://github.com/ekanshuj/youtube-ui-clone",
+    name: "url-shortener",
+    url: "https://github.com/ekanshuj/url-shortener",
     description:
-      "UI clone of the youTube website using vanilla javaScript, replicating the YouTube website's user interface, utilizing HTML and SCSS to achieve the design layout and styling",
+      "A React.js application that allows users to shorten long URLs into shorter, more manageable links and uses a API serviceto do so, allowing users to copy them to their clipboard.",
   },
   {
     icon: dots,
     name: "oaiig",
     url: "https://github.com/ekanshuj/oaiig",
     description:
-      "Open AI Image Generator is a web application that leverages the OpenAI DALL-E 2 API to generate images based on user-defined prompts using AI",
+      "Open AI Image Generator is a web application that leverages the OpenAI DALL-E 2 API to generate images based on user-defined prompts using AI.",
   },
   {
     icon: dots,
     name: "scrapper-api",
     url: "https://github.com/ekanshuj/scrapper-api",
     description:
-      "A web server Proxy API to crawl web pages over the internet with a simple API call for a variety of purposes, such as data mining, data analysis, or data visualization",
+      "A web server Proxy API to crawl web pages over the internet with a simple API call for a variety of purposes, such as data mining, data analysis, or data visualization.",
   },
   {
     icon: dots,
